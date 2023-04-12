0274bf5c6c96fbf42f05671dc281027b786ba5fa
Ryan <ryan.1603@outlook.com>
2023-04-11 22:50:58 -0400

    First command added

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-testing
+import discord
+import os
+
+client = discord.client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(msg):
+    if msg.author == client.user:
+        return
+    
+    if msg.content.startswith('!sup'):
+        await msg.channel.send('Sup!')
